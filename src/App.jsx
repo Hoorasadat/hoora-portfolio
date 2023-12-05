01136fefdef48f661a30234467d76d3374933300
Hoora <hoora@Hooras-MacBook-Pro.local>
2023-12-05 12:45:12 -0700

    fixing scroll to top upon refreshing

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Main from './sections/Main';
 
@@ -19,30 +19,30 @@ const router = createBrowserRouter([
 function App() {
 
   // Scroll to the top when the component mounts
+  const [scrollToTop, setScrollToTop] = useState(false);
 
   useEffect(() => {
-    const handleScroll = () => {
+    if (scrollToTop) {
       window.scrollTo(0, 0);
+
+      // Reset the state to false to avoid infinite loops
+      setScrollToTop(false);
+    }
+  }, [scrollToTop]);
+
+  useEffect(() => {
+    const handleBeforeUnload = () => {
+      // Set the state to true when the page is about to unload (before refresh)
+      setScrollToTop(true);
     };
 
-    // Add event listener for scrolling
-    window.addEventListener('scroll', handleScroll);
+    window.addEventListener('beforeunload', handleBeforeUnload);
 
-    // Check if the page is being reloaded
-    if (window.performance && window.performance.navigation.type === 1) {
-      // Delay scrolling to the top
-      setTimeout(() => {
-        window.scrollTo(0, 0);
-        // Remove the event listener after scrolling
-        window.removeEventListener('scroll', handleScroll);
-      }, 1000);
-    }
-
-    // Cleanup the event listener on component unmount
     return () => {
-      window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener('beforeunload', handleBeforeUnload);
     };
   }, []);
+
 
 
   return (
