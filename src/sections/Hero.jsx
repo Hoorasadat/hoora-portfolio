1f143de361d79a7d482c6871fbb226c961290771
Hoora <hoora@Hooras-MacBook-Pro.local>
2023-12-05 13:05:07 -0700

    edit hero

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -2,18 +2,19 @@ import React from 'react';
 import { Link } from 'react-scroll';
 import classNames from 'classnames';
 import styles from './Hero.module.scss';
-import btnStyles from '../common/styles/_buttons.scss';
-import fontStyles from '../common/styles/_typography.scss';
 
 const Hero = () => {
 	return (
 		<section className={classNames(styles.hero, 'jumbotron')}>
 			<div className="container">
-				<h1 className={classNames(styles.heroTitle, styles.loadHidden)}>
-					<span className="textColorMain">Hoora Mirsanei</span>
-					<br />
+				<p className={classNames(styles.heroTitle, styles.loadHidden, "textColorMain")}>
+					Hoora Mirsanei
+				</p>
+
+				<p className={classNames(styles.heroPosition, styles.loadHidden)}>
 					Full-Stack Software Developer
-				</h1>
+				</p>
+
 				<p className={classNames(styles.heroCta, styles.loadHidden)}>
 					<Link rel="noreferrer" to="about" className="ctaBtn ctaBtnHero" smooth={true} duration={500} >
 						More about me!
