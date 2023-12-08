c6bc6efe81155e4bf8af52b42422b85d25cb2391
Hoora <hoora@Hooras-MacBook-Pro.local>
2023-12-08 11:50:37 -0700

    make hero responsive

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import { Link } from 'react-scroll';
 import classNames from 'classnames';
 import styles from './Hero.module.scss';
-import heroImage from '../../assets/heroImage.jpeg'
+import heroImage from '../../assets/heroImage.jpeg';
 
 const Hero = () => {
 	return (
-		<section className={classNames(styles.hero, "jumbotron", "d-flex", "col-md")}>
+		<section className={classNames(styles.hero, "jumbotron", "d-flex", "flex-md-row", "flex-column-reverse")}>
 			<div className="col-md-6">
 				<p className={classNames(styles.heroTitle, styles.loadHidden, "textColorMain")}>
 					Hoora Mirsanei
@@ -17,7 +17,7 @@ const Hero = () => {
 				</p>
 
 				<p className={classNames(styles.heroCta, styles.loadHidden)}>
-					<Link rel="noreferrer" to="about" className="ctaBtn ctaBtnHero" smooth={true} duration={500} >
+					<Link rel="noreferrer" to="about" className="ctaBtn ctaBtnHero" smooth={true} duration={500}>
 						More about me!
 					</Link>
 				</p>
@@ -25,7 +25,7 @@ const Hero = () => {
 
 			<div className="col-md-6">
 				<img
-					className="rounded-circle img-fluid"
+					className="rounded-circle img-fluid m-1"
 					src={heroImage}
 					alt="Unknown"
 				/>
