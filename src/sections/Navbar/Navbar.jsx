c5d96154d2e74e35c8c65ffb2966deeb5f6c5d58
Hoora <hoora@Hooras-MacBook-Pro.local>
2023-12-22 13:13:59 -0700

    adding navbar special styling

--- src/sections/Navbar/Navbar.jsx
+++ src/sections/Navbar/Navbar.jsx
@@ -5,101 +5,117 @@ import logo from '../../assets/logo.png';
 import styles from './Navbar.module.scss';
 
 const NavHeader = () => {
-  const [bounceAnimation, setBounceAnimation] = useState(false);
+	const [isScrolled, setIsScrolled] = useState(false);
+  	const [bounceAnimation, setBounceAnimation] = useState(false);
 
-  useEffect(() => {
-	// Start the bounce animation after component mount
-	const intervalId = setInterval(() => {
-	  setBounceAnimation((prev) => !prev);
-	}, 500);
+	useEffect(() => {
+		const handleScroll = () => {
+			const scrollPosition = window.scrollY;
+			setIsScrolled(scrollPosition > 500);
+		};
 
-	// Clear the interval when the component unmounts
-	return () => clearInterval(intervalId);
-  }, []);
+		// Add scroll event listener
+		window.addEventListener('scroll', handleScroll);
 
-  return (
-	<nav className={classNames(styles.nav, "navbar fixed-top navbar-expand-lg")}>
-		<button
-			className="navbar-toggler"
-			type="button"
-			data-toggle="collapse"
-			data-target="#navbarNav"
-			aria-controls="navbarNav"
-			aria-expanded="false"
-			aria-label="Toggle navigation"
-		>
-			<span className="nav navbar-toggler-icon"></span>
-		</button>
-		<div className="collapse navbar-collapse" id="navbarNav">
-			<ul className="navbar-nav ml-auto">
+		// Remove event listener when component unmounts
+		return () => {
+			window.removeEventListener('scroll', handleScroll);
+		};
+	}, []);
 
-				<li className="nav-item">
-					<Link
-						id="len1"
-						className={classNames(styles.navlink, "navbar-brand", { bounce: bounceAnimation })}
-						rel="noreferrer"
-						to="home"
-						smooth={true}
-						duration={500}
-					>
-						<img className={styles.logo} src={logo} alt="HOME" />
-					</Link>
-				</li>
-				<li className="nav-item">
-					<Link
-						id="len2"
-						className={classNames(styles.navlink, styles.navText, "nav-link")}
-						rel="noreferrer"
-						to="about"
-						smooth={true}
-						duration={500}
-					>
-					ABOUT
-					</Link>
-				</li>
+  	useEffect(() => {
+		// Start the bounce animation after component mount
+		const intervalId = setInterval(() => {
+		setBounceAnimation((prev) => !prev);
+		}, 500);
 
-				<li className="nav-item">
-					<Link
-						id="len3"
-						className={classNames(styles.navlink, styles.navText, "nav-link")}
-						rel="noreferrer"
-						to="resume"
-						smooth={true}
-						duration={500}
-					>
-						RESUME
-					</Link>
-				</li>
+		// Clear the interval when the component unmounts
+		return () => clearInterval(intervalId);
+	}, []);
 
-				<li className="nav-item">
-					<Link
-						id="len4"
-						className={classNames(styles.navlink, styles.navText, "nav-link")}
-						rel="noreferrer"
-						to="projects"
-						smooth={true}
-						duration={500}
-					>
-						PORTFOLIO
-					</Link>
-				</li>
+	return (
+		<nav className={classNames(styles.nav, "navbar fixed-top navbar-expand-lg", isScrolled && styles.scrolled)}>
+			<button
+				className="navbar-toggler"
+				type="button"
+				data-toggle="collapse"
+				data-target="#navbarNav"
+				aria-controls="navbarNav"
+				aria-expanded="false"
+				aria-label="Toggle navigation"
+				>
+				<span className="nav navbar-toggler-icon"></span>
+			</button>
+			<div className="collapse navbar-collapse" id="navbarNav">
+				<ul className="navbar-nav ml-auto">
 
-				<li className="nav-item">
-					<Link
-						id="len5"
-						className={classNames(styles.navlink, styles.navText, "nav-link")}
-						rel="noreferrer"
-						to="contact"
-						smooth={true}
-						duration={500}
-					>
-						CONTACT
-					</Link>
-				</li>
-			</ul>
-		</div>
-	</nav>
-  );
+					<li className="nav-item">
+						<Link
+							id="len1"
+							className={classNames(styles.navlink, "navbar-brand", { bounce: bounceAnimation })}
+							rel="noreferrer"
+							to="home"
+							smooth={true}
+							duration={500}
+						>
+							<img className={styles.logo} src={logo} alt="HOME" />
+						</Link>
+					</li>
+					<li className="nav-item">
+						<Link
+							id="len2"
+							className={classNames(styles.navlink, styles.navText, isScrolled && styles.scrolled, "nav-link")}
+							rel="noreferrer"
+							to="about"
+							smooth={true}
+							duration={500}
+						>
+							ABOUT
+						</Link>
+					</li>
+
+					<li className="nav-item">
+						<Link
+							id="len3"
+							className={classNames(styles.navlink, styles.navText, isScrolled && styles.scrolled, "nav-link")}
+							rel="noreferrer"
+							to="resume"
+							smooth={true}
+							duration={500}
+						>
+							RESUME
+						</Link>
+					</li>
+
+					<li className="nav-item">
+						<Link
+							id="len4"
+							className={classNames(styles.navlink, styles.navText, isScrolled && styles.scrolled, "nav-link")}
+							rel="noreferrer"
+							to="projects"
+							smooth={true}
+							duration={500}
+						>
+							PORTFOLIO
+						</Link>
+					</li>
+
+					<li className="nav-item">
+						<Link
+							id="len5"
+							className={classNames(styles.navlink, styles.navText, isScrolled && styles.scrolled, "nav-link")}
+							rel="noreferrer"
+							to="contact"
+							smooth={true}
+							duration={500}
+						>
+							CONTACT
+						</Link>
+					</li>
+				</ul>
+			</div>
+		</nav>
+	);
 };
 
 export default NavHeader;
